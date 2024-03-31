bbdbd306ba095ffe845a90b5d84700f1c3f4650f
PeyaPeyaPeyang <peyang@peya.tokyo>
2024-03-31 18:43:50 +0900

    perf: Optimize build process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
 
-# Function to install dependencies
-build_one_nms() {
-    version=$1
-
-    # Create directory for build
-    mkdir -p "nms-build/${version//./_}"
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to create directory for version $version"
-        return 1
-    fi
-    cd "nms-build/${version//./_}" || return 1
-
+download_build_tools() {
     # Download BuildTools.jar
     wget -O BuildTools.jar "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
     if [ $? -ne 0 ]; then
         echo "Error: Failed to download BuildTools.jar for version $version"
-        cd ..
         return 1
     fi
+}
 
+# Function to install dependencies
+build_one_nms() {
+    version=$1
     # Determine Java version based on provided Minecraft version
     case $version in
         1.17 | 1.16* | 1.15* | 1.14* | 1.13* | 1.12* | 1.11* | 1.10* | 1.9* | 1.8*)
@@ -42,11 +34,8 @@ build_one_nms() {
     "$JAVA_EXE" -jar BuildTools.jar --rev "$version"
     if [ $? -ne 0 ]; then
         echo "Error: Failed to build version $version"
-        cd ..
         return 1
     fi
-
-    cd ../..
 }
 
 # Check if versions are provided as arguments
@@ -58,17 +47,24 @@ fi
 # Join the comma-separated list into a space-separated list
 version_list=$(echo "$*" | tr ',' ' ')
 
+# Create directory for build
+mkdir -p "nms-build"
+cd "nms-build" || exit 1
+
+# Download BuildTools.jar
+download_build_tools || exit 1
+
 # Iterate through provided versions
 for version in $version_list; do
     build_one_nms "$version" || exit 1
 done
 
 # Install to github .m2
-mkdir -p nms-build/.m2/repository
+mkdir -p .m2/repository
 if [ $? -ne 0 ]; then
     echo "Error: Failed to create .m2/repository directory"
     exit 1
 fi
 
 # Deploy to local
-cp -r /root/.m2/repository/. nms-build/.m2/repository || exit 1
+cp -r /root/.m2/repository .m2/repository || exit 1
